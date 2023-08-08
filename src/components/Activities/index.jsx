f1b75bffc737ae3f89ae851d66b9e36121b903dd
ptahemdjehuty <pariso03henri@gmail.com>
2023-08-08 18:00:05 +0100

    ➕ add activities component

---
+++ src/components/Activities/index.jsx
@@ -0,0 +1,89 @@
+export default function Activities() {
+  return (
+    <>
+      <>
+        {/*====== Start Features Section ======*/}
+        <section className="features-section pt-100">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-xl-8">
+                {/*=== Section Title ===*/}
+                <div className="section-title text-center mb-45 wow fadeInDown">
+                  <span className="sub-title">Tendance</span>
+                  <h2>Nos activités récentes</h2>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-xl-3 col-md-6 col-sm-12">
+                {/*=== Features Image Item ===*/}
+                <div className="single-features-item-two mb-40 wow fadeInUp">
+                  <div className="img-holder">
+                    <img
+                      src="assets/images/features/feat-5.jpg"
+                      alt="Features Image"
+                    />
+                    <div className="item-overlay">
+                      <div className="content">
+                        <h3 className="title">Safari</h3>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-xl-3 col-md-6 col-sm-12">
+                {/*=== Features Image Item ===*/}
+                <div className="single-features-item-two mb-40 wow fadeInDown">
+                  <div className="img-holder">
+                    <img
+                      src="assets/images/features/feat-6.jpg"
+                      alt="Features Image"
+                    />
+                    <div className="item-overlay">
+                      <div className="content">
+                        <h3 className="title">Randonnée</h3>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-xl-3 col-md-6 col-sm-12">
+                {/*=== Features Image Item ===*/}
+                <div className="single-features-item-two mb-40 wow fadeInUp">
+                  <div className="img-holder">
+                    <img
+                      src="assets/images/features/feat-7.jpg"
+                      alt="Features Image"
+                    />
+                    <div className="item-overlay">
+                      <div className="content">
+                        <h3 className="title">Pêche</h3>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-xl-3 col-md-6 col-sm-12">
+                {/*=== Features Image Item ===*/}
+                <div className="single-features-item-two mb-40 wow fadeInDown">
+                  <div className="img-holder">
+                    <img
+                      src="assets/images/features/feat-8.jpg"
+                      alt="Features Image"
+                    />
+                    <div className="item-overlay">
+                      <div className="content">
+                        <h3 className="title">Découvertes</h3>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/*====== End Features Section ======*/}
+      </>
+    </>
+  );
+}
